33d08b9b5f475294523c9392bfc8b9c4df27d4a7
Tanawit Pattanaveerangkoon <tanawit.pat@gmail.com>
2019-08-17 21:35:31 +0700

    Edit log messages

--- app.py
+++ app.py
@@ -1,23 +1,23 @@
 import os
 from PIL import Image
  
-def resize_image(input_image_path, output_image_path, size):
+def resize_image(file_name, input_image_path, output_image_path, size):
     original_image = Image.open(input_image_path)
     width, height = original_image.size
-    print('The original image size is {wide} wide x {height} '
-          'high'.format(wide=width, height=height))
+    print('{file_name} - The original image size is {wide} wide x {height} '
+          'high'.format(file_name=file_name, wide=width, height=height))
  
     resized_image = original_image.resize(size)
     width, height = resized_image.size
-    print('The resized image size is {wide} wide x {height} '
-          'high'.format(wide=width, height=height))
+    print('{file_name} - The resized image size is {wide} wide x {height} '
+          'high'.format(file_name=file_name, wide=width, height=height))
     resized_image.save(output_image_path)
  
 def remove_output_files():
     outputFileList = os.listdir('output')
     for file in outputFileList:
-        os.unlink("output/{}".format(file))
-        print("{} has been removed".format(file))
+        os.unlink("output/{file_name}".format(file_name=file))
+        print("{file_name} has been removed".format(file_name=file))
 
 if __name__ == '__main__':
     width = 600
@@ -29,6 +29,7 @@ if __name__ == '__main__':
     for file in inputFileList:
         if not file[0] == ".":
             resize_image(
+                file_name=file,
                 input_image_path="input/{}".format(file),
                 output_image_path='output/{}'.format(file),
                 size=(width, height)
